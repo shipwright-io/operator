39c86f08a40c5af6e32761c93aef9cc2b00bbdb6
Otávio Fernandes <otaviof@gmail.com>
2021-06-02 16:58:11 +0200

    Multi-platform hack scripts
    Adding extra variables (OS and ARCH) based on Golang's, in order to
    support more operating systems and architectures in hack scripts.

--- hack/install-operator-sdk.sh
+++ hack/install-operator-sdk.sh
@@ -1,7 +1,27 @@
-#! /bin/bash
+#!/usr/bin/env bash
+#
+# Download and copy opeartor-sdk to the desired location. The target location is specified by the
+# first argument. For instance:
+#
+# $ install-operator-sdk.sh "bin/operator-sdk"
+#
 
 set -e
 
-dest=${1:-bin/operator-sdk}
-curl -sL -o "${dest}" https://github.com/operator-framework/operator-sdk/releases/download/v1.4.2/operator-sdk_linux_amd64
-chmod +x "${dest}"
+DEST="${1:-bin/operator-sdk}"
+SDK_VERSION="${SDK_VERSION:-1.4.2}"
+
+OS="${OS:-linux}"
+ARCH="${ARCH:-amd64}"
+
+SDK_URL_HOST="${SDK_HOST:-github.com}"
+SDK_URL_PATH="${SDK_URL_PATH:-operator-framework/operator-sdk/releases/download}"
+SDK_URL="https://${SDK_URL_HOST}/${SDK_URL_PATH}/v${SDK_VERSION}/operator-sdk_${OS}_${ARCH}"
+
+if [ -x ${DEST} ] ; then
+    echo "# operator-sdk is already installed at '${DEST}'"
+    exit 0
+fi
+
+curl --silent --location --output "${DEST}" "${SDK_URL}"
+chmod +x "${DEST}"
