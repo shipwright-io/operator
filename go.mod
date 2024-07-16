533e37e7d2ef66cd90ed142d9b7b95fd488abe2f
Adam Kaplan <adam.kaplan@redhat.com>
2024-07-16 07:21:10 -0400

    migrate pkg/tekton
    Migrate pkg/tekton to internal/pkg/tekton

--- go.mod
+++ go.mod
@@ -3,22 +3,34 @@ module github.com/shipwright-io/operator
 go 1.20
 
 require (
+	github.com/go-logr/logr v1.2.4
+	github.com/manifestival/controller-runtime-client v0.4.0
+	github.com/manifestival/manifestival v0.7.2
 	github.com/onsi/ginkgo/v2 v2.11.0
 	github.com/onsi/gomega v1.27.10
+	github.com/tektoncd/operator v0.67.0
+	gopkg.in/yaml.v2 v2.4.0
+	k8s.io/api v0.28.3
+	k8s.io/apiextensions-apiserver v0.28.3
 	k8s.io/apimachinery v0.28.3
-	k8s.io/client-go v0.28.3
+	k8s.io/client-go v1.5.2
 	sigs.k8s.io/controller-runtime v0.16.3
 )
 
 require (
+	contrib.go.opencensus.io/exporter/ocagent v0.7.1-0.20200907061046-05415f1de66d // indirect
+	contrib.go.opencensus.io/exporter/prometheus v0.4.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/blendle/zapdriver v1.3.1 // indirect
+	github.com/census-instrumentation/opencensus-proto v0.4.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/emicklei/go-restful/v3 v3.11.0 // indirect
 	github.com/evanphx/json-patch v5.6.0+incompatible // indirect
 	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/go-logr/logr v1.2.4 // indirect
+	github.com/go-kit/log v0.2.1 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/go-logr/zapr v1.2.4 // indirect
 	github.com/go-openapi/jsonpointer v0.19.6 // indirect
 	github.com/go-openapi/jsonreference v0.20.2 // indirect
@@ -32,45 +44,68 @@ require (
 	github.com/google/gofuzz v1.2.0 // indirect
 	github.com/google/pprof v0.0.0-20210720184732-4bb14d4b1be1 // indirect
 	github.com/google/uuid v1.3.0 // indirect
-	github.com/imdario/mergo v0.3.10 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.2 // indirect
+	github.com/hashicorp/golang-lru v0.5.4 // indirect
+	github.com/imdario/mergo v0.3.14 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/manifestival/controller-runtime-client v0.4.0 // indirect
-	github.com/manifestival/manifestival v0.7.2 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/openshift-pipelines/pipelines-as-code v0.19.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/prometheus/statsd_exporter v0.23.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tektoncd/pipeline v0.47.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.25.0 // indirect
-	golang.org/x/exp v0.0.0-20220722155223-a9213eeb770e // indirect
+	golang.org/x/exp v0.0.0-20230321023759-10a507213a29 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sync v0.2.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/term v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.9.3 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
+	google.golang.org/api v0.119.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230526161137-0005af68ea54 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20230525234035-dd9d682886f9 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230525234030-28d5490b6b19 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
 	google.golang.org/protobuf v1.30.0 // indirect
 	gopkg.in/inf.v0 v0.9.1 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.28.3 // indirect
-	k8s.io/apiextensions-apiserver v0.28.3 // indirect
 	k8s.io/component-base v0.28.3 // indirect
 	k8s.io/klog/v2 v2.100.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20230717233707-2695361300d9 // indirect
 	k8s.io/utils v0.0.0-20230406110748-d93618cff8a2 // indirect
+	knative.dev/pkg v0.0.0-20230320014357-4c84b1b51ee8 // indirect
 	sigs.k8s.io/json v0.0.0-20221116044647-bc3834ca7abd // indirect
 	sigs.k8s.io/structured-merge-diff/v4 v4.2.3 // indirect
 	sigs.k8s.io/yaml v1.3.0 // indirect
 )
+
+// Go modules at times does not effectively resolve transitive dependencies that share
+// k8s.io/* packages. These should be kept internally consistent with a "common" k8s version - this
+// is more of an art than a science, and can change with successive dependency updates.
+//
+// Each replace here should provide a comment explaining why it is warranted, and its necessity
+// should be tested with successive dependency updates.
+
+// k8s.io/client-go has a "latest" semantic version of v1.5.2, which predates the project
+// go module version standardization (v0.y.z, with y and z representing the k8s 1.y.z minor/patch
+// versions). `go mod tidy` will often overwrite the desired client-go version to v1.5.2, so we
+// pin the version here.
+replace k8s.io/client-go => k8s.io/client-go v0.28.3
---
+++ internal/pkg/tekton/tekton_test.go
@@ -0,0 +1,448 @@
+package tekton
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/shipwright-io/operator/internal/pkg/common"
+
+	o "github.com/onsi/gomega"
+
+	tektonoperatorv1alpha1 "github.com/tektoncd/operator/pkg/apis/operator/v1alpha1"
+	tektonoperatorfake "github.com/tektoncd/operator/pkg/client/clientset/versioned/fake"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	apiextensionsfake "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/fake"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/version"
+	ktesting "k8s.io/client-go/testing"
+)
+
+func TestReconcileTekton(t *testing.T) {
+	cases := []struct {
+		name                           string
+		taskRunCRD                     *apiextensionsv1.CustomResourceDefinition
+		tektonConfigCRD                *apiextensionsv1.CustomResourceDefinition
+		tektonConfigObj                *tektonoperatorv1alpha1.TektonConfig
+		createTektonConfigErr          error
+		expectError                    bool
+		expectRequeue                  bool
+		expectTektonConfigCreateAction bool
+	}{
+		{
+			name:        "No Tekton Objects",
+			expectError: true,
+		},
+		{
+			name: "Tekton TaskRuns defined",
+			taskRunCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "taskruns.tekton.dev",
+				},
+			},
+		},
+		{
+			name: "TektonConfig defined no version",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+				},
+			},
+			expectRequeue: true,
+			expectError:   true,
+		},
+		{
+			name: "TektonConfig defined insufficient version",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": "v0.36.0",
+					},
+				},
+			},
+			expectRequeue: true,
+			expectError:   true,
+		},
+		{
+			name: "TektonConfig defined sufficient version",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": common.TektonOpMinSupportedVersion,
+					},
+				},
+			},
+			expectTektonConfigCreateAction: true,
+		},
+		{
+			name: "Create TektonConfig error",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": common.TektonOpMinSupportedVersion,
+					},
+				},
+			},
+			createTektonConfigErr:          fmt.Errorf("internal error CREATE"),
+			expectError:                    true,
+			expectRequeue:                  true,
+			expectTektonConfigCreateAction: true,
+		},
+		{
+			name: "TektonConfig already created",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": common.TektonOpMinSupportedVersion,
+					},
+				},
+			},
+			tektonConfigObj: &tektonoperatorv1alpha1.TektonConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "config",
+				},
+			},
+			expectRequeue: false,
+			expectError:   false,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			g := o.NewWithT(t)
+			ctx := context.TODO()
+			crds := []runtime.Object{}
+			if tc.taskRunCRD != nil {
+				crds = append(crds, tc.taskRunCRD)
+			}
+			if tc.tektonConfigCRD != nil {
+				crds = append(crds, tc.tektonConfigCRD)
+			}
+			crdClient := apiextensionsfake.NewSimpleClientset(crds...)
+			tektonConfigs := []runtime.Object{}
+			if tc.tektonConfigObj != nil {
+				tektonConfigs = append(tektonConfigs, tc.tektonConfigObj)
+			}
+			tektonOperatorClient := tektonoperatorfake.NewSimpleClientset(tektonConfigs...)
+			if tc.createTektonConfigErr != nil {
+				tektonOperatorClient.PrependReactor("create", "tektonconfigs", func(action ktesting.Action) (handled bool, ret runtime.Object, err error) {
+					return true, nil, tc.createTektonConfigErr
+				})
+			}
+			tektonConfig, requeue, err := ReconcileTekton(ctx, crdClient.ApiextensionsV1(), tektonOperatorClient.OperatorV1alpha1())
+			if tc.expectError {
+				g.Expect(err).To(o.HaveOccurred())
+			} else {
+				g.Expect(err).NotTo(o.HaveOccurred())
+			}
+			g.Expect(requeue).To(o.Equal(tc.expectRequeue))
+			createOccurred := false
+			for _, action := range tektonOperatorClient.Actions() {
+				if action.Matches("create", "tektonconfigs") {
+					createOccurred = true
+				}
+			}
+			g.Expect(createOccurred).To(o.Equal(tc.expectTektonConfigCreateAction))
+			if tc.expectTektonConfigCreateAction && tc.createTektonConfigErr == nil {
+				g.Expect(tektonConfig).NotTo(o.BeNil())
+				g.Expect(tektonConfig.Name).To(o.Equal("config"))
+				g.Expect(tektonConfig.Spec.Profile).To(o.Equal("lite"))
+				g.Expect(tektonConfig.Spec.TargetNamespace).To(o.Equal("tekton-pipelines"))
+			}
+		})
+	}
+}
+
+func TestIsTektonPipelinesInstalled(t *testing.T) {
+	cases := []struct {
+		name            string
+		expectInstalled bool
+		expectError     bool
+	}{
+		{
+			name: "Not present",
+		},
+		{
+			name:            "Tekton present",
+			expectInstalled: true,
+		},
+		{
+			name:        "Error getting TaskRuns",
+			expectError: true,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.TODO()
+			objects := []runtime.Object{}
+			if tc.expectInstalled {
+				objects = append(objects, &apiextensionsv1.CustomResourceDefinition{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "taskruns.tekton.dev",
+					},
+				})
+			}
+			client := apiextensionsfake.NewSimpleClientset(objects...)
+			if tc.expectError {
+				client.PrependReactor("get", "customresourcedefinitions", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, nil, fmt.Errorf("test error!")
+				})
+			}
+			installed, err := IsTektonPipelinesInstalled(ctx, client.ApiextensionsV1())
+			if tc.expectError {
+				if err == nil {
+					t.Error("expected error to be raised, but none was received")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if installed != tc.expectInstalled {
+				t.Errorf("expected installed to be %t, got %t", tc.expectInstalled, installed)
+			}
+
+		})
+	}
+}
+
+func TestIsTektonOperatorInstalled(t *testing.T) {
+	cases := []struct {
+		name            string
+		expectInstalled bool
+		expectError     bool
+	}{
+		{
+			name: "Not present",
+		},
+		{
+			name:            "Tekton Operator present",
+			expectInstalled: true,
+		},
+		{
+			name:        "Error getting TetkonConfigs",
+			expectError: true,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.TODO()
+			objects := []runtime.Object{}
+			if tc.expectInstalled {
+				objects = append(objects, &apiextensionsv1.CustomResourceDefinition{
+					ObjectMeta: metav1.ObjectMeta{
+						Name: "tektonconfigs.operator.tekton.dev",
+					},
+				})
+			}
+			client := apiextensionsfake.NewSimpleClientset(objects...)
+			if tc.expectError {
+				client.PrependReactor("get", "customresourcedefinitions", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, nil, fmt.Errorf("test error!")
+				})
+			}
+			installed, err := IsTektonOperatorInstalled(ctx, client.ApiextensionsV1())
+			if tc.expectError {
+				if err == nil {
+					t.Error("expected error to be raised, but none was received")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if installed != tc.expectInstalled {
+				t.Errorf("expected installed to be %t, got %t", tc.expectInstalled, installed)
+			}
+
+		})
+	}
+}
+
+func TestGetTektonOperatorVersion(t *testing.T) {
+	cases := []struct {
+		name            string
+		tektonConfigCRD *apiextensionsv1.CustomResourceDefinition
+		expectedVersion *version.Version
+		thrownError     error
+		expectError     bool
+	}{
+		{
+			name:        "No CRD",
+			expectError: true,
+		},
+		{
+			name:        "Get CRD error",
+			thrownError: fmt.Errorf("failed to GET"),
+			expectError: true,
+		},
+		{
+			name: "No labels on TektonConfig CRD",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "No release label on TektonConfig CRD",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"some-label": "value",
+					},
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "release label on TektonConfig CRD is not a semver",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": "value",
+					},
+				},
+			},
+			expectError: true,
+		},
+		{
+			name: "Valid TektonConfig CRD",
+			tektonConfigCRD: &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "tektonconfigs.operator.tekton.dev",
+					Labels: map[string]string{
+						"operator.tekton.dev/release": common.TektonOpMinSupportedVersion,
+					},
+				},
+			},
+			expectedVersion: version.MustParseSemantic(common.TektonOpMinSupportedVersion),
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.TODO()
+			objects := []runtime.Object{}
+			if tc.tektonConfigCRD != nil {
+				objects = append(objects, tc.tektonConfigCRD)
+			}
+			client := apiextensionsfake.NewSimpleClientset(objects...)
+			if tc.thrownError != nil {
+				client.PrependReactor("*", "*", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, nil, tc.thrownError
+				})
+			}
+			version, err := GetTektonOperatorVersion(ctx, client.ApiextensionsV1())
+			if tc.expectError {
+				if err == nil {
+					t.Error("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+			if !reflect.DeepEqual(version, tc.expectedVersion) {
+				t.Errorf("expected version %s, got %s", tc.expectedVersion, version)
+			}
+		})
+	}
+}
+
+func TestIsTektonConfigPresent(t *testing.T) {
+	cases := []struct {
+		name          string
+		tektonConfig  *tektonoperatorv1alpha1.TektonConfig
+		thrownError   error
+		expectPresent bool
+		expectError   bool
+	}{
+		{
+			name: "No TektonConfig",
+		},
+		{
+			name:        "List TektonConfig error",
+			thrownError: fmt.Errorf("failed to LIST"),
+			expectError: true,
+		},
+		{
+			name: "TektonConfig present",
+			tektonConfig: &tektonoperatorv1alpha1.TektonConfig{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "config",
+				},
+			},
+			expectPresent: true,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			ctx := context.TODO()
+			objects := []runtime.Object{}
+			if tc.tektonConfig != nil {
+				objects = append(objects, tc.tektonConfig)
+			}
+			client := tektonoperatorfake.NewSimpleClientset(objects...)
+			if tc.thrownError != nil {
+				client.PrependReactor("*", "*", func(action ktesting.Action) (bool, runtime.Object, error) {
+					return true, nil, tc.thrownError
+				})
+			}
+			isPresent, err := IsTektonConfigPresent(ctx, client.OperatorV1alpha1())
+			if tc.expectError {
+				if err == nil {
+					t.Error("expected error, got nil")
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("unexpected error %v", err)
+			}
+			if isPresent != tc.expectPresent {
+				t.Errorf("expected TektonConfig presence to be %t, got %t", tc.expectPresent, isPresent)
+			}
+		})
+	}
+}
+
+func TestCreateTektonConfig(t *testing.T) {
+	ctx := context.TODO()
+	client := tektonoperatorfake.NewSimpleClientset()
+	expectedProfile := "test"
+	expectedNamespace := "test-namespace"
+	tektonConfig, err := CreateTektonConfigWithProfileAndTargetNamespace(ctx,
+		client.OperatorV1alpha1(),
+		expectedProfile,
+		expectedNamespace)
+	if len(client.Actions()) != 1 {
+		t.Errorf("expected 1 client action, got %d", len(client.Actions()))
+	}
+	for _, action := range client.Actions() {
+		if !action.Matches("create", "tektonconfigs") {
+			t.Errorf("expected action %s/%s, got %s/%s", "create", "tektonconfigs", action.GetVerb(), action.GetResource().Resource)
+		}
+	}
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if tektonConfig == nil {
+		t.Fatal("received nil TektonConfig")
+	}
+	if tektonConfig.Name != "config" {
+		t.Errorf("expected TektonConfig name %s, got %s", "config", tektonConfig.Name)
+	}
+	if tektonConfig.Spec.Profile != expectedProfile {
+		t.Errorf("expected profile %s, got %s", expectedProfile, tektonConfig.Spec.Profile)
+	}
+	if tektonConfig.Spec.TargetNamespace != expectedNamespace {
+		t.Errorf("expected target namespace %s, got %s", expectedNamespace, tektonConfig.Spec.TargetNamespace)
+	}
+}
